855c84bb3c3027b30c8800e3ca0c63288774e3de
haachicanoy <harold22010@gmail.com>
2018-03-06 07:59:32 -0500

    Image processing updates

--- _semester_2/_foundations_computational_vision/_codes/image_processing.R
+++ _semester_2/_foundations_computational_vision/_codes/image_processing.R
@@ -1,4 +1,4 @@
-# Image processing
+# Foundations of computer vision: Image processing
 # By: Harold Achicanoy
 # 2018
 
@@ -18,6 +18,7 @@ library(schoolmath)
 photos <- list.files(path = "./photos", full.names = T)
 photos <- list.files(path = "./segmented_photos", full.names = T)
 
+
 # Read images: identify format
 image_read <- function(file = "./photos/IMG_5901.JPG"){
   aux <- imager::load.image(file)[,,1][1,1]
@@ -28,11 +29,11 @@ image_read <- function(file = "./photos/IMG_5901.JPG"){
   }
   return(img)
 }
-img <- image_read(file = photos[1])
-plot(img)
+img <- image_read(file = "C:/Users/haachicanoy/Documents/elephant.jpg") # img <- image_read(file = photos[1])
+ggRGB(raster::brick(raster(t(img[,,1])), raster(t(img[,,2])), raster(t(img[,,3]))), 1, 2, 3, stretch = "lin", q = 0)
 
 
-# Sampling to the middle
+# DOWN-SAMPLING
 image_sampling <- function(img = img){
   
   img_c1 <- matrix(data = NA, nrow = dim(img)[1]/2, ncol = dim(img)[2]/2, byrow = T)
@@ -58,9 +59,9 @@ image_sampling <- function(img = img){
     
   }
   
-  r <- raster::raster(img_c1) # Red
-  g <- raster::raster(img_c2) # Green
-  b <- raster::raster(img_c3) # Blue
+  r <- raster::raster(t(img_c1)) # Red
+  g <- raster::raster(t(img_c2)) # Green
+  b <- raster::raster(t(img_c3)) # Blue
   img_r <- raster::brick(r, g, b)
   names(img_r) <- c("r", "g", "b")
   
@@ -71,7 +72,7 @@ img_s <- image_sampling(img = img)
 ggRGB(img_s, 1, 2, 3, stretch = "lin", q = 0)
 
 
-# Quantization
+# LINEAR QUANTIZATION: 8 to 4 bits
 # 0-15 -> 0
 # 16-31 -> 1
 # ...
@@ -89,9 +90,9 @@ image_quantization <- function(img = img){
   img.2_q <- matrix(data = as.numeric(as.character(cut(x = img.2, bits8_cuttoff, 0:16))), nrow = dim(img.1)[1], ncol = dim(img.1)[2], byrow = F)
   img.3_q <- matrix(data = as.numeric(as.character(cut(x = img.3, bits8_cuttoff, 0:16))), nrow = dim(img.1)[1], ncol = dim(img.1)[2], byrow = F)
   
-  r <- raster::raster(img.1_q) # Red
-  g <- raster::raster(img.2_q) # Green
-  b <- raster::raster(img.3_q) # Blue
+  r <- raster::raster(t(img.1_q)) # Red
+  g <- raster::raster(t(img.2_q)) # Green
+  b <- raster::raster(t(img.3_q)) # Blue
   img_r <- raster::brick(r, g, b)
   names(img_r) <- c("r", "g", "b")
   
@@ -101,7 +102,8 @@ image_quantization <- function(img = img){
 img_q <- image_quantization(img = img)
 ggRGB(img_q, 1, 2, 3, stretch = "lin", q = 0)
 
-# Convolution
+
+# CONVOLUTION
 generate_kernel <- function(values = values, size = 3){
   kernel <- matrix(data = values, nrow = size, ncol = size, byrow = T)
   plot(raster::raster(kernel))
@@ -119,6 +121,12 @@ kernel <- generate_kernel(values = c(0.0625, 0.125, 0.0625,
 kernel <- generate_kernel(values = c(-1, -2, -1,
                                      0, 0, 0,
                                      1, 2, 1), size = 3)
+# Mean filter
+kernel <- generate_kernel(values = c(1/9, 1/9, 1/9,
+                                     1/9, 1/9, 1/9,
+                                     1/9, 1/9, 1/9), size = 3)
+
+
 image_convolution <- function(img = img, kernel = kernel){
   
   k.size <- ncol(kernel)
@@ -149,9 +157,9 @@ image_convolution <- function(img = img, kernel = kernel){
     
   }
   
-  r <- raster::raster(img_conv1) # Red
-  g <- raster::raster(img_conv2) # Green
-  b <- raster::raster(img_conv3) # Blue
+  r <- raster::raster(t(img_conv1)) # Red
+  g <- raster::raster(t(img_conv2)) # Green
+  b <- raster::raster(t(img_conv3)) # Blue
   img_conv <- raster::brick(r, g, b)
   names(img_conv) <- c("r", "g", "b")
   
@@ -173,9 +181,64 @@ gaussian_filter <- function(sigma = 1, k.size = 5){
   }
   return(kernel)
 }
-kernel <- gaussian_filter(sigma = 3, k.size = 3)
+kernel <- gaussian_filter(sigma = 3, k.size = 7)
+plot(raster(kernel))
 img_conv <- image_convolution(img = img, kernel = kernel)
 ggRGB(img_conv, 1, 2, 3, stretch = "lin", q = 0)
+
+# For implementing: sigma, Nagao-Matsuyama, impulse noise and median filter
+
+
+# COLOR TRANSFORMATIONS: RGB to CIE XYZ
+rgb2xyz <- function(img = img){
+  
+  img.1 <- img[,,1]
+  img.2 <- img[,,2]
+  img.3 <- img[,,3]
+  
+  x <- 0.412453 * img.1 + 0.357580 * img.2 + 0.180423 * img.3
+  y <- 0.212671 * img.1 + 0.715160 * img.2 + 0.072169 * img.3
+  z <- 0.019334 * img.1 + 0.119193 * img.2 + 0.950227 * img.3
+  
+  x <- raster::raster(t(x)) # Red
+  y <- raster::raster(t(y)) # Green
+  z <- raster::raster(t(z)) # Blue
+  img_xyz <- raster::brick(x, y, z)
+  names(img_xyz) <- c("x", "y", "z")
+  
+  return(img_xyz)
+  
+}
+img_xyz <- rgb2xyz(img = img)
+ggRGB(img_xyz, 1, 2, 3, stretch = "lin", q = 0)
+
+plot(RGBtoXYZ(img)) # XYZ
+plot(XYZtoLab(RGBtoXYZ(img))) # Lab
+plot(RGBtoYUV(img)) # YUV
+plot(RGBtoYCbCr(img)) # YCbCr
+plot(RGBtoHSV(img)) # HSV
+plot(RGBtoHSL(img)) # HSL
+
+plot(RGBtoHSI(img)) # HSI
+plot(RGBtosRGB(img)) # sRGB
+
+# Image histogram
+hist(img[,,2])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # setwd("E:/Buenas-1342/Iguales/P_abajo")
 # photos <- list.files(path = ".", full.names = T)
